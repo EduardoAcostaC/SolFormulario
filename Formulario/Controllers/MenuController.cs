680a4ce4ae32876a4d779ff84268611c02063493
EduardoAcostaC <eacostac1700@alumno.ipn.mx>
2023-08-03 17:57:10 -0600

    Agregar archivos de proyecto.

---
+++ Formulario/Controllers/MenuController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Formulario.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Index()
+        {
+            return View("Inicio");
+        }
+
+        public ActionResult FormularioDatos()
+        {
+            return View("FormularioDatos");
+        }
+
+        public ActionResult FormularioDireccion()
+        {
+            return View("FormularioDireccion");
+        }
+
+        public ActionResult FormularioPOST(string Nombre, string Paterno, string Materno, string Edad)
+        {
+            string mensajeUno = $"Mi nombre es {Nombre} {Paterno} {Materno} y tengo {Edad} años";
+            TempData["tempdata1"] = mensajeUno;
+
+            return View("FormularioDatos");
+        }
+
+        public ActionResult FormularioPOST2(string Direccion, string Colonia, string Delegacion)
+        {
+            string mensajeDos = $"{Direccion} {Colonia} {Delegacion}";
+            TempData["tempdata2"] = mensajeDos;
+
+            return View("VistaDireccion");
+        }
+    }
+}
